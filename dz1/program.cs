bba896cd40606881ecefaadda48c620d9fae1f93
HooBaWork <121292182+HooBaWork@users.noreply.github.com>
2023-02-07 19:27:59 +0300

    Create program.cs

---
+++ dz1/program.cs
@@ -0,0 +1,39 @@
+/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.Clear();
+Console.WriteLine("Введите размер массива");//Записываем числа пользователя в массив через функцию returnArray() .Пользователь сам указывает размер массива через консоль.
+int size = int.Parse(Console.ReadLine());
+int[] resultArray = returnArray();
+int[] returnArray()
+{
+
+   int[] array = new int[size];
+   for(int i = 0;i < size;i++)
+   {
+    
+    Console.WriteLine("Введите число");
+    array[i] = int.Parse(Console.ReadLine());
+   }
+   return array;
+}
+
+
+int result(int[] arr) //Отправляем получившийся массив в функцию и прогоняем через цикл,в поисках элементов массива,которые больше 0.
+{
+   int count = 0;
+   for(int i = 0;i < size;i++)
+    if(resultArray[i] > 0)
+    {
+        count++;
+    }
+    return count;
+} 
+
+void main()  //С оформлением main,да и вообще с форматированием кода ещё разбираться и разбираться ...
+{
+int res = result(resultArray);
+Console.WriteLine($"Количество элементов массива больше 0 -  {res}");
+}
+main();
